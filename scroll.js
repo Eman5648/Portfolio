2b3c128384c7c57d42625d6923440d01a75b9c28
Eman5648 <x00152620@mytudublin.ie>
2021-12-12 00:53:50 +0000

    new homepage

--- scroll.js
+++ scroll.js
@@ -1,21 +1,49 @@
-const boxes = document.querySelectorAll(".box");
+window.onload = function () {
+  Particles.init({
+    // normal options
+    selector: '.background',
+    maxParticles: 110,
+    color: '#FFFFFF',
+    connectParticles: true,
 
-window.addEventListener("scroll", checkBoxes);
-
-checkBoxes();
-
-function checkBoxes() {
-    const triggerBottom = (window.innerHeight / 5) * 4;
-
-    boxes.forEach((box) => {
-        const boxTop = box.getBoundingClientRect().top;
-
-        if (boxTop < triggerBottom) {
-            box.classList.add("show");
-        } else {
-            box.classList.remove("show");
+    // options for breakpoints
+    responsive: [
+      {
+        breakpoint: 768,
+        options: {
+          maxParticles: 60,
+          color: '#FFFFFF',
+          connectParticles: true
         }
-    });
-}
+      }
+      , {
+        breakpoint: 525,
+        options: {
+          maxParticles: 40,
+          connectParticles: true
+        }
+      }, {
+        breakpoint: 320,
+        options: {
+          maxParticles: 25
+        }
+      }
+    ]
+  });
+};
 
+$(document).ready(function(){
+    var typed = new Typed(".typing", {
+                            strings: ["Student", "Software Developer", "Web Developer", "Data Analyst", "Network Engineer"],
+                            typeSpeed: 100,
+                            backSpeed: 100,
+                            loop: true
+                            
+                        
+                            
+                    });
+                });
+function formResponse() {
+                        alert("Thank you, Your message is sent. I will get back to you as soon as possible.");
+}
 
